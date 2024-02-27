0567288904052bc8bfa5a6b15ae185a7abb3f488
Reflektomer-hub <larinya567a@gmail.com>
2024-02-27 16:53:46 +0300

    Начало проекта. Создание основного решения.

---
+++ Sec_P2P_code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec_P2P_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
